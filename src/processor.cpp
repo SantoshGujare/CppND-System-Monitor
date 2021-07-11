dd15dca43fe64abd7fbad62507a07680b41188c5
Santosh Gujare <85542228+SantoshGujare@users.noreply.github.com>
2021-07-11 22:58:44 +0530

    Update processor.cpp
    updated file to calculate total CPU utilization.

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,4 +1,9 @@
 #include "processor.h"
-
+#include "linux_parser.h"
 // TODO: Return the aggregate CPU utilization
-float Processor::Utilization() { return 0.0; }
+float Processor::Utilization() 
+{ 
+  float total_cpu_utilization {0};
+  total_cpu_utilization = (float)LinuxParser::ActiveJiffies()/LinuxParser::Jiffies();
+  return (total_cpu_utilization); 
+}
